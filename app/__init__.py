df632269dceb77bb980dbedf5a398efb341c5147
irmscher9 <irmscher9@users.noreply.github.com>
2018-11-30 16:12:59 +0200

    + forms.py, models.py, sqlalchemy, flask-login, flask-migrate, gunicorn

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = '/database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '1c42e22df7b4d8bb636c328a2ffcd6fe990402ae125c2a07'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+
 
 from app import routes
